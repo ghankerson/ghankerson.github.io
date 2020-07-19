b21a9c4eb10cb028177851b07e1292bed6447fd3
Geoff Hankerson <ghankerson@mor.org>
2020-07-19 15:35:11 -0500

    start adding content

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 export default function Home () {
   return (
     <div>
-      Hello World, again.{' '}
-      <Link href='/about' as={process.env.BACKEND_URL + '/about'}>
-        <a>About</a>
+      <p> This is my booknotes sites where I share notes on books I am reading and podcasts I am listening to.</p>
+      <Link href='/johnbetts' as={process.env.BACKEND_URL + '/johnbetts'}>
+        <a>Jung's Model of the Psyche</a>
       </Link>
     </div>
   )
