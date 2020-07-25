2e7d01c2c979e128667b0abc1454f65c18deacb3
Geoff <ghankstef@gmail.com>
2020-07-25 11:07:27 -0500

    add out dir

--- deploy.js
+++
@@ -1,8 +0,0 @@
-const ghpages = require('gh-pages');
-
-ghpages.publish('out', {
-  branch: 'master',
-  repo: 'https://github.com/ghankerson/ghankerson.github.io.git'
-}, () => {
-  console.log('Deployed!');
-});
