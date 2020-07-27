b85ecee3e20ae07142d5b9de6abf9e486266fa49
Geoff Hankerson <ghankerson@mor.org>
2020-07-27 10:23:33 -0500

    all typescript

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
 
-
-const Nav= () => {
-	return (
-	  <ul className="navigation">
-	    <li>
-	      <Link href="/books">
-	        <a>Book Notes</a>
-	      </Link>
-	    </li>
-	    <li>
-	      <Link href="/podcasts">
-	        <a>Podcast Notes</a>
-	      </Link>
-	    </li>
-	  </ul>
-	)
+const Nav = () => {
+  return (
+    <ul className='navigation'>
+      <li>
+        <Link href='/books'>
+          <a>Book Notes</a>
+        </Link>
+      </li>
+      <li>
+        <Link href='/podcasts'>
+          <a>Podcast Notes</a>
+        </Link>
+      </li>
+    </ul>
+  )
 }
 
 export default Nav
