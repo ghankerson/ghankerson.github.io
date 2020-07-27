d12d21b22435967c83e3bbfb3d23f5142cf2f528
Geoff Hankerson <ghankerson@mor.org>
2020-07-27 10:51:22 -0500

    Add Attribution for Enemies podcast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/fonts.css'
 import '../styles/layout.css'
 import '../styles/elements.css'
 import '../styles/navbar.css'
+import '../styles/components/attribution.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp ({ Component, pageProps }) {
