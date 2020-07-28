1f977a12752f1f67ade07da027563b2bb0379b20
Geoff Hankerson <ghankerson@mor.org>
2020-07-27 22:24:07 -0500

    Deploy to Github pages

---
+++ out/_next/static/webpack/static/development/pages/index.js.2bec7528f43b13b6226e.hot-update.js
@@ -0,0 +1,16 @@
+webpackHotUpdate("static/development/pages/index.js",[
+/* 0 */,
+/* 1 */
+false,
+/* 2 */
+/*!*****************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fghankerson%2Fcode%2Fbooknotes%2Fpages%2Findex.mdx ***!
+  \*****************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fghankerson%2Fcode%2Fbooknotes%2Fpages%2Findex.mdx! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fghankerson%2Fcode%2Fbooknotes%2Fpages%2Findex.mdx!./");
+
+
+/***/ })
+])
---
+++ out/_next/static/webpack/static/development/pages/origins/uroboros.js.af7521943ffcf46f5f48.hot-update.js
@@ -0,0 +1,18 @@
+webpackHotUpdate("static/development/pages/origins/uroboros.js",[
+/* 0 */,
+/* 1 */,
+/* 2 */,
+/* 3 */
+false,
+/* 4 */
+/*!************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Forigins%2Furoboros&absolutePagePath=%2FUsers%2Fghankerson%2Fcode%2Fbooknotes%2Fpages%2Forigins%2Furoboros.mdx ***!
+  \************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Forigins%2Furoboros&absolutePagePath=%2FUsers%2Fghankerson%2Fcode%2Fbooknotes%2Fpages%2Forigins%2Furoboros.mdx! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Forigins%2Furoboros&absolutePagePath=%2FUsers%2Fghankerson%2Fcode%2Fbooknotes%2Fpages%2Forigins%2Furoboros.mdx!./");
+
+
+/***/ })
+])
