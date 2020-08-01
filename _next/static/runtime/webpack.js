9f6a2cb8acc76ffed3a8a03e2cc2f6eeca2978cf
Geoff Hankerson <ghankerson@mor.org>
2020-08-01 11:26:43 -0500

    Deploy to Github pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "324fef108f012e628a45";
+/******/ 	var hotCurrentHash = "c0f43fb3add77e3467b4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
