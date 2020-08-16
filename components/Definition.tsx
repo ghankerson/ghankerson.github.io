b9ef4280e8bb77e72d384b3cd6c475e1ef343275
Geoff Hankerson <ghankerson@mor.org>
2020-08-16 17:47:05 -0500

    fix typescript

--- components/Definition.tsx
+++ components/Definition.tsx
@@ -17,13 +17,13 @@ const Definition = ( { label, text, source } ) => {
   return (
     <dl className={styles.definiton}>
       <dt className={styles.term} >
-        <a role='link' tabIndex='-1' onKeyPress={() => toggler()} onClick={() => toggler()}>{label}</a>
+        <a role='link' tabIndex={-1} onKeyPress={() => toggler()} onClick={() => toggler()}>{label}</a>
       </dt>
       <dd className={styles[ToggleClass]}>
         <div>{text} <br /> <br /> </div>
         <div><em>{source}</em></div>
         <div>
-          <a role='link' tabIndex='0' onKeyPress={() => toggler()} onClick={() => toggler()}>Close</a>
+          <a role='link' tabIndex={0} onKeyPress={() => toggler()} onClick={() => toggler()}>Close</a>
           <br /> 
         </div>
       </dd>
